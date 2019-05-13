f4ed6cbb379da4f1705e13e10fb582a2b7de5a18
Josh Soref <jsoref@gmail.com>
2019-05-12 22:12:33 -0400

    spelling: retrieving

--- jcl/src/openj9.traceformat/share/classes/com/ibm/jvm/trace/format/api/TraceRecord.java
+++ jcl/src/openj9.traceformat/share/classes/com/ibm/jvm/trace/format/api/TraceRecord.java
@@ -387,7 +387,7 @@ public class TraceRecord implements Comparable<TraceRecord> {
 						guardBytes = stream.getGuardBytes();
 						if (found != missing.length) {
 							if (context.debugStream != null) {
-								context.debug(this, 4, "discarding spanned long trace point special because we failed retriving "+missing.length+"bytes, got "+found);
+								context.debug(this, 4, "discarding spanned long trace point special because we failed retrieving "+missing.length+"bytes, got "+found);
 							}
 							discard = true;
 							break;
