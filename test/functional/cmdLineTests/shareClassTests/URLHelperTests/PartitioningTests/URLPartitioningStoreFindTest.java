1c5aeb75b6e074190163fb5631775b278a9aa992
Josh Soref <jsoref@gmail.com>
2019-05-14 02:00:22 -0400

    spelling: specify

--- test/functional/cmdLineTests/shareClassTests/URLHelperTests/PartitioningTests/URLPartitioningStoreFindTest.java
+++ test/functional/cmdLineTests/shareClassTests/URLHelperTests/PartitioningTests/URLPartitioningStoreFindTest.java
@@ -42,7 +42,7 @@ public class URLPartitioningStoreFindTest {
 		
 		if(args.length != 2){
 			System.out.println("\n Incorrect usage");
-			System.out.println("\n Please specifiy -testfile <filename>");
+			System.out.println("\n Please specify -testfile <filename>");
 		}
 		
 		URLPartitioningStoreFindTest test = new URLPartitioningStoreFindTest();
