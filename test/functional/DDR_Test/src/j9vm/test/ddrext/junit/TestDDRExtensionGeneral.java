2a417b14284a18304049be4e1b9a1864ef8714e7
Josh Soref <jsoref@users.noreply.github.com>
2021-06-01 03:50:15 -0400

    spelling: collected
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- test/functional/DDR_Test/src/j9vm/test/ddrext/junit/TestDDRExtensionGeneral.java
+++ test/functional/DDR_Test/src/j9vm/test/ddrext/junit/TestDDRExtensionGeneral.java
@@ -445,7 +445,7 @@ public class TestDDRExtensionGeneral extends DDRExtTesterBase {
 				Constants.DCLIBS_FAILURE_KEYS, false));
 		
 		//if library is collected to the core, then test "!dclibs extract" command to extract libs to temp folder
-		if (output.contentEquals(Constants.DCLIBS_LIB_COLLENTED)){
+		if (output.contentEquals(Constants.DCLIBS_LIB_COLLECTED)){
 			String output2 = exec(Constants.DCLIBS_CMD, new String[] {"extract" });
 			assertTrue(validate(output2, Constants.DCLIBS_EXTRACT_SUCCESS_KEYS,
 					Constants.DCLIBS_FAILURE_KEYS, false));
